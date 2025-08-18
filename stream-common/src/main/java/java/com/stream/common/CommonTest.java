b1c52bf2644e0f417f60ecda3a9ecce11015b5bd
ljs3492586109 <3492586109@qq.com>
2025-08-18 14:46:26 +0800

    add

---
+++ stream-common/src/main/java/java/com/stream/common/CommonTest.java
@@ -0,0 +1,17 @@
+package java.com.stream.common;
+
+import com.stream.common.utils.ConfigUtils;
+
+/**
+
+ * @description: Test
+ */
+public class CommonTest {
+
+    public static void main(String[] args) {
+        String kafka_err_log = ConfigUtils.getString("kafka.err.log");
+        System.err.println(kafka_err_log);
+    }
+
+
+}
